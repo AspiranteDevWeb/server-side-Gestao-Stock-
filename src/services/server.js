2222558e836b46baf019a6b2f0a95f75a235de4b
Damaiceno Castro <cenocastro01@gmail.com>
2023-10-19 08:34:23 +0200

    configuring server file

--- src/services/server.js
+++ src/services/server.js
@@ -2,7 +2,13 @@ import express from "express";
 
 const app = express()
 
+const cors=require("cors")
+const mysql=require("mysql2")
+
 const Porta=3000 || process.env.PORT
+
+app.use(cors())
+app.use(express.json())
 
 app.listen(Porta,()=>{
     console.log("Servidor inicializado na porta ",  `${Porta}`)
