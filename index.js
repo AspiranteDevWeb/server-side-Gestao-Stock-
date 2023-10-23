64dfcf751db0d0711d34c6b145d7e2e3c2ee04c9
Damaiceno Castro <cenocastro01@gmail.com>
2023-10-23 10:01:15 +0200

    inproving index.js file that is the main file response for calling all back-end files

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const express=require("express")
-const app=express()
-const cors=require("cors")
-const mysql=require("mysql2")
-
-const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:""
-})
- const port= 3000 || process.env.PORT
-app.use(cors())
-app.use(express.json())
-
-app.listen(port,()=>{
-    console.log("Servidor iniciado na porta ", `${port}`)
-})
+require("./src/services/server")
