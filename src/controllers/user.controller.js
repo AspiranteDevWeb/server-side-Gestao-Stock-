0cf2d16e3cb37d33dcc1d59b37c6d1496e9fb740
Damaiceno Castro <cenocastro01@gmail.com>
2023-10-30 13:11:00 +0200

    updating user SQL requests...

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,6 +23,7 @@ const db = require('../database/conect')
         })
     }
 
+
     exports.usuario_por_nome=(req,res)=>{
         const {nome}=req.params;
 
@@ -44,6 +45,7 @@ const db = require('../database/conect')
         }
     }
 
+
     exports.usuarios_por_email=(req,res)=>{
         const {email}=req.params
 
@@ -61,9 +63,63 @@ const db = require('../database/conect')
 
 
     exports.actualiza_dados_usuario=(req,res)=>{
+        const {id,nome,email,senha1,senha2,funcao}=req.body
         try{
-            if(!email || !nome){
+
+            if(email==undefined || email==null){
                 console.log("")
+            }else{
+                let SQL ="UPDATE usuario SET email =? WHERE id =?";
+
+                db.db.query(SQL,[email,id],(err,result)=>{
+                    if(err)console.log(err,"")
+                    else console.log(result)
+                })
+                
+            }
+
+            if(nome==undefined || nome == null){
+                console.log("")
+            }else{
+                let SQL="UPDATE usuario SET nome=? WHERE id=?"
+
+                db.db.query(SQL,[nome,id],(err,result)=>{
+                    if(err) console.log(err)
+                    else console.log(result)
+                })
+            }
+
+            if(senha1 == undefined || senha1 == null){
+                console.log("")
+            }else{
+                let SQL="UPDATE usuario SET senha1 = ? WHERE id = ?";
+
+                db.db.query(SQL,[senha1,id],(err,result)=>{
+                    if(err) console.log(err)
+                    else console.log(result)
+                })
+            }
+
+            if(senha2 == undefined ||senha2 == null){
+                console.log("")
+            }else{
+                let SQL="UPDATE usuario SET senha2=? WHERE id=?";
+
+                db.db.query(SQL,[senha2,id],(err,result)=>{
+                    if(err)console.log(err)
+                    else console.log(result)
+                })
+            }
+
+            if(funcao == undefined || funcao ==null){
+                console.log("")
+            }else{
+                let SQL="UPDATE usuario SET funcao=? WHERE id=?";
+
+                db.db.query(SQL,[funcao,id],(err,result)=>{
+                    if(err)console.log(err)
+                    else console.log(result)
+                })
             }
         }catch(err){
             console.log("")
@@ -75,9 +131,11 @@ const db = require('../database/conect')
         const {nome, email, senha1, senha2, funcao}=req.body;
         try{
             let SQL = "INSERT INTO usuario (nome, email, senha1, senha2, funcao) values (?,?,?,?,?)"
-            db.db.query(SQL,[], (err,result)=>{
+            db.db.query(SQL,[nome, email, senha1, senha2, funcao], (err,result)=>{
                 //db.release()para terminar requisicoes
-                console.log(err,"")
+                if(err)console.log(err,"")
+
+                //else res.status(200)
             })
         }catch(err){
             console.log(err,"o Erro...")
@@ -96,7 +154,7 @@ const db = require('../database/conect')
 
            
         }catch(err){
-            console.log('dtfygh')
+            console.log(err,"dtfygh")
         }
     }
 
