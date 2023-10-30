97a7607d4403d69d686e31b3e67a5a7f61f4bfd0
Damaiceno Castro <cenocastro01@gmail.com>
2023-10-30 02:47:55 +0200

    updating all methods

--- src/controllers/produt.controller.js
+++ src/controllers/produt.controller.js
@@ -1,5 +1,61 @@
 const db=require('../database/conect')
 
+exports.receber_tudo=(req,res)=>{
+    let SQL="SELECT * FROM produto INNER JOIN armazem WHERE id=id";
+
+    db.db.query(SQL,(err,result)=>{
+        if(err)console.log(err,"")
+
+        else res.status(200).json(result)
+    })
+}
+
+exports.receber_tudo_por_id=(req,res)=>{
+    
+    let SQL="SELECT * FROM produto INNER JOIN armazem ON id=id WHERE Id=?";
+
+    db.db.query(SQL,[id],(err,result)=>{
+        if(err)console.log(err)
+        else res.status(200).json(result)
+    })
+}
+
+exports.recebe_tudo_por_nome=(req,res)=>{
+
+    let SQL="SELECT * FROM produto INNER JOIN armazem ON id=id WHERE id=?";
+
+    db.db.query(SQL,[nome],(err,result)=>{
+        if(err) console.log(err)
+
+        else res.status(200).json(result)
+    })
+}
+
+exports.id_nome=(req,result)=>{
+
+    let SQL="SELECT * FROM produto INNER JOIN armazem on id=id where id=? and nome=?";
+
+    db.db.query(SQL,[id,nome],(err,result)=>{
+        if(err) console.log(err)
+
+        else res.status(200).json(result)
+    })
+}
+
+exports.soma_tempo_total=(req,res)=>{
+
+    let SQL="select sec_to_time(sum(time_to_sec(tempo))) as tempototal from produto";
+
+    db.db.query(SQL,(err,result)=>{
+        if(err)console.log(err)
+
+        else res.end(JSON.stringifye(result))
+    })
+}
+
+
+
+
 exports.inserir_novo_produto_armazem=(req,res)=>{
     const {sala,gaveta,pratileira,corredor}=req.body;
 
@@ -36,13 +92,17 @@ exports.inserir_novo_produto_armazem=(req,res)=>{
 exports.update_produto_armazem=(req,res)=>{
     const {armazem_idarmazem, produto_nome, produto_formato, data_emissao, tempo, produto_observacao}=req.body;
 
-    if(!armazem_idarmazem && !produto_nome && produto_formato, !data_emissao && !tempo && !produto_observacao){
-        console.log("")
-    }else{
+    const {sala, gaveta, pratileira, corredor}=req.body;
+
+    
         
         if (produto_nome != null && produto_nome != undefined){
             try{
-                let SQL=""
+                let SQL="UPDATE produto SET produto_nome=? Where armazem_idarmazem=?";
+                db.db.query(SQL,[produto_nome,armazem_idarmazem],(err,result)=>{
+                    if(err) console.log(err,"")
+                    else console.log(result,"")
+                })
             }catch(err){
                 console.log(err)
             }
@@ -75,7 +135,35 @@ exports.update_produto_armazem=(req,res)=>{
                 console.log(err)
             }
         }
-    }
+        if(sala != null && sala !=undefined){
+            try{
+                let SQL=""
+            }catch (err){
+                console.log(err)
+            }
+        }
+        if(gaveta != null && gaveta !=undefined){
+            try{
+                let SQL=""
+            }catch(err){
+                console.log(err)
+            }
+        } 
+        if(pratileira != null && pratileira !=undefined){
+            try{
+                let SQL =""
+            }catch(err){
+                console.log(err)
+            }
+        }
+        if(corredor != null && corredor !=undefined){
+            try{
+                let SQL="";
+            }catch(err){
+                console.log(err)
+            }
+        }
+    
 }
 
 
