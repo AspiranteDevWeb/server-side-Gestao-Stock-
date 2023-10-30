b31abe163f97aae49bd7cf4b811c84a3b49e3398
Damaiceno Castro <cenocastro01@gmail.com>
2023-10-30 13:18:14 +0200

    removing require mysql2 at this file

--- src/services/server.js
+++ src/services/server.js
@@ -1,12 +1,18 @@
 import express from "express";
+
+const dotenv = require('dotenv')
+dotenv.config() 
+//require('dotenv').config() se nao funcionar use isto
 import router from "../routes/router";
 
 const app = express()
 
 const cors=require("cors")
-const mysql=require("mysql2")
+//const mysql=require("mysql2")
 
-const Porta=3000 || process.env.PORT
+ 
+//const Porta=3000 || process.env.PORT
+const Porta=3000 || `${process.env.PORT}`
 
 app.use(cors())
 app.use(express.json())
