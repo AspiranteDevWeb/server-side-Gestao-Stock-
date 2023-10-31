2d37bf7667d2fd30ceadc05683c8b95aa12972f1
Damaiceno Castro <cenocastro01@gmail.com>
2023-10-31 14:00:25 +0200

    Need to configure the environment variables for database connection

--- src/database/conect.js
+++ src/database/conect.js
@@ -1,10 +1,14 @@
 const mysql=require("mysql2")
 
 const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:""
+    host:"localhost",  //=process.env.MYSQL_HOST
+    user:"root",  //==process.env.MYSQL_USER
+    password:"",  //==process.env.MYSQL_PASSWORD
+    database:"bd_sistema_gestao_2023"  //==process.env.MYSQL_DATABASE
 })
 
 module.exports= {db}
+
+//exports.db=db;
+
+//module.exports=db
