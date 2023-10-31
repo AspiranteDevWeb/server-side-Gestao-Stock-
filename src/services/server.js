ddab3fe326ba03f304e31fa39f79af3f8d8ca81d
Damaiceno Castro <cenocastro01@gmail.com>
2023-10-31 12:59:42 +0200

    This file is responsible for connecting the system to the browser and the rest of the system files. It uses a middleware, app.use(). The app.use() is a middleware that is used to control the user's requests to the database and vice-versa.  A middleware is a function that needs to have a request, response, and optionally a next() to terminate the function

--- src/services/server.js
+++ src/services/server.js
@@ -1,15 +1,17 @@
-import express from "express";
-
+//import express from "express";
+const express=require("express")
 const dotenv = require('dotenv')
 dotenv.config() 
 //require('dotenv').config() se nao funcionar use isto
-import router from "../routes/router";
-
+//import router from "../routes/router";
+//const router = require("../routes/router")
 const app = express()
 
 const cors=require("cors")
 //const mysql=require("mysql2")
 
+
+//require("../routes/router") //================================
  
 //const Porta=3000 || process.env.PORT
 const Porta=3000 || `${process.env.PORT}`
@@ -17,7 +19,13 @@ const Porta=3000 || `${process.env.PORT}`
 app.use(cors())
 app.use(express.json())
 
-app.use(router)
+//app.use(router)//middleware
+app.use(require("../routes/router"),(res,req,next)=>{
+    if (err)console.log(err)
+    else console.log(res)
+    next()
+})//=====================
+
 
 app.listen(Porta,()=>{
     console.log("Servidor inicializado na porta ",  `${Porta}`)
