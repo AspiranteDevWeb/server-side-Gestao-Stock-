28ef572468c2ce0ec8d53b5c26c6462c869dffa1
Damaiceno Castro <cenocastro01@gmail.com>
2023-10-31 12:45:30 +0200

    This current file is responsible for storing the routas executed on the system. In order to be able to share this entire file, we must export it as a module, using : module.exports=router

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,26 +1,30 @@
-import { Router } from "express";
+const express=require("express")
+//import { Router } from "express";
+//const Router=require(".Router")
 const controller = require('../controllers/user.controller')
 const controller1= require('../controllers/produt.controller')
-//const UserModels = require("UserModels")
-import UserModels from "../models/user.models";
+//import { Router } from "express";
+//const UserModels = require("../models/user.models")
 
-const UserModels=new UserModels()
+//import UserModels from "../models/user.models";
+//const UserModels=require("../controllers")
+//const UserModels=new UserModels()
 
-const router=Router()
+const router=express.Router()
 
 
 //users routers
 router.get("/buscar_usuario",controller.todos_usuarios)
 
-router.get("/buscar_usuario",UserModels.todos_usuarios())
+//router.get("/buscar_usuario",UserModels.todos_usuarios()) <=> desse jeito ele nao espera ser chamado e executa logo
 
-router.get("/buscar_usuario_id:levar_nome",UserModels.usuario_por_nome())
+router.get("/buscar_usuario_id/:nome",controller.usuario_por_nome)
 
-router.get("/buscar_usuario_id:email",UserModels.usuarios_por_email())
+router.get("/buscar_usuario_id/:email",controller.usuarios_por_email)
 
-router.put("/update_usuario:id",UserModels.actualiza_dados_usuario())
+router.put("/update_usuario/:id",controller.actualiza_dados_usuario)
 
-router.post("/signup_novo_usuario",UserModels.inserir_novo_usuario())
+router.post("/signup_novo_usuario",controller.inserir_novo_usuario)
 
 router.delete("/delete_usuario",controller.apagar_usuario)
 //router.get("/awsredtf",(req,res)=>{})
@@ -28,21 +32,21 @@ router.delete("/delete_usuario",controller.apagar_usuario)
 
 //produts routes
 
-router.get("/produto_armazem",controller1.receber_tudo())
-router.get("/armazem_armazem/:nome",controller1.recebe_tudo_por_nome())
-router.get("/produto_armazem/:id",controller1.receber_tudo_por_formato())
-router.get("/produto_armazem/:id/:nome",controller1.receber_tudo_por_formato_nome())
-router.get("/somaTempoTotal/",controller1.soma_tempo_total())
+router.get("/produto_armazem",controller1.receber_tudo)
+router.get("/armazem_armazem/:nome",controller1.recebe_tudo_por_nome)
+router.get("/produto_armazem/:id",controller1.receber_tudo_por_formato)
+router.get("/produto_armazem/:id/:nome",controller1.receber_tudo_por_formato_nome)
+router.get("/somaTempoTotal/",controller1.soma_tempo_total)
 
-router.post("/novo_produto_armazem",controller1.inserir_novo_produto_armazem())
-router.put("/actualiza_produto_armazem/:id",controller1.update_produto_armazem())
-router.delete("/delete_produto_armazem/:id",controller1.apagar_produto_armazem())
+router.post("/novo_produto_armazem",controller1.inserir_novo_produto_armazem)
+router.put("/actualiza_produto_armazem/:id",controller1.update_produto_armazem)
+router.delete("/delete_produto_armazem/:id",controller1.apagar_produto_armazem)
 //ou
 
-router.get("/",UserModels.getNomeTodosUsuarios())
+//router.get("/",UserModels.getNomeTodosUsuarios)
 
-export default router
-
+//exports.router
+module.exports=router
 
 /*
 API com Node.JS - Passo 2 - Criando as rotas (Atualizado)
